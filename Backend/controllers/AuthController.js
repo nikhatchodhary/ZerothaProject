fc03d2f1e19cbb43dbeddb7fa717d5afd06c5450
nikhatchodhary <choudharynikhat74@gmail.com>
2025-10-20 13:34:07 +0530

    Update backend

--- Backend/controllers/AuthController.js
+++ Backend/controllers/AuthController.js
@@ -13,7 +13,7 @@ module.exports.Signup = async (req, res, next) => {
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
       withCredentials: true,
-      httpOnly: false,
+     sameSite:"none",
     });
     res
       .status(201)
@@ -41,7 +41,7 @@ module.exports.Login = async (req, res, next) => {
      const token = createSecretToken(user._id);
      res.cookie("token", token, {
        withCredentials: true,
-       httpOnly: false,
+       sameSite:"none",
      });
      res.status(201).json({ message: "User logged in successfully", success: true });
      next()
