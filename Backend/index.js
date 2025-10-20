ab080d77e870f750e08691369593cea19ba93dad
nikhatchodhary <choudharynikhat74@gmail.com>
2025-10-20 12:53:51 +0530

    Update backend

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const app=express();
 
 app.use(cors({
 origin: [
-  "hhttps://zerotha-0pnu.onrender.com",
+  "https://zerotha-0pnu.onrender.com",
   "https://zerodha-dashboard.onrender.com",
   "http://localhost:5173",
   "http://localhost:5174"
