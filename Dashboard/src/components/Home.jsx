075ceaaedfb6c9a91e149fe775cb6dd43ff8bfd9
nikhatchodhary <choudharynikhat74@gmail.com>
2025-10-21 14:09:33 +0530

    update code

--- Dashboard/src/components/Home.jsx
+++ Dashboard/src/components/Home.jsx
@@ -50,7 +50,7 @@ function Home(){
     autoClose: 1500,
   });
   setTimeout(() => {
-    window.location.replace("https://zerotha-0pnu.onrender.com/login");
+    window.location.href="https://zerotha-0pnu.onrender.com/login";
   }, 1500);
 };
     
